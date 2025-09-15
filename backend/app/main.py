056fb6fe3864d8805ca3392b8c45ae57a1bb5f11
Avi1874772 <avinash.1874772@studenti.uniroma1.it>
2025-09-15 16:26:56 +0200

    changes in main and activated the virtual env

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+# create a FastAPI instance
+app = FastAPI(title="CSA Research Chatbot", version="0.1.0")
 
+# Root endpoint - just for testing
 @app.get("/")
 def root():
     return {"message": "Backend is running"}
