df2d657a3c4542791eb501990ee33c0c968b8d03
Avi1874772 <avinash.1874772@studenti.uniroma1.it>
2025-11-24 17:00:52 +0100

    changes in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,4 +4,45 @@ from pydantic import BaseModel
 import os
 
 from .config import Settings
-from .chatbot import answer_wuery
+from .chatbot import answer_query
+
+# ----------APPLICATION COMPONENTS-----------
+app = FastAPI(
+    title="CSA-Chatbot",
+    description="A chatbot for the CSA project",
+    version="0.0.1",
+)
+
+# ----------CORS CONFIGURATION-----------
+origins = [
+    "http://localhost:8080",
+    "http://10.199.1.77:8080/",
+    "http://172.23.0.1:8080/",
+    
+    # Original FastAPI/Vite defaults
+    "http://localhost:5173",        
+    "http://127.0.0.1:5173",        
+    "http://localhost:8000",
+]
+origins = [origin for origin in origins if origin]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# ------------ENDPOINTS-------------
+@app.get("/")
+def root():
+    return {"message": "Backend is running"}
+
+class ChatRequest(BaseModel):
+    query: str
+
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+    reponse = answer_query(request.query)
+    return {"response": reponse}
