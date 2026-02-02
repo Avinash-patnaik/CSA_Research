2a42ce7c3d6c51861ae7ec71344bc13ab63b9a84
Avinash-patnaik <avinashpatnaik1001@gmail.com>
2026-02-02 16:48:16 +0100

    changes in chatbot

--- backend/app/chatbot.py
+++ backend/app/chatbot.py
@@ -1,6 +1,6 @@
-from .utils.llm_local import get_local_llm_response
+from .utils.llm_hf import get_hf_response, get_local_llm_response
 
 def answer_query(user_query: str) -> str:
-    llm_response =get_local_llm_response(user_query)
+    llm_response =get_hf_response(user_query)
     
     return llm_response
